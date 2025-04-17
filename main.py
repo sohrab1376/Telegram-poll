af5e0d17106779dd2419cac0cc214a7054bf36c1
sohrab1376 <sohrab.shabab@gmail.com>
2025-04-18 02:11:18 +0330

    Update main.py

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ async def get_results(update: Update, context: ContextTypes.DEFAULT_TYPE):
     with open(RESPONSES_FILE, "r", encoding="utf-8") as f:
         data = json.load(f)
 
-    message = "نتایج ثبت‌شده:
+    message = "نتایج ثبت‌شده:"
 
 "
     for uid, info in data.items():
